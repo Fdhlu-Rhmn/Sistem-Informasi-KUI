f4bdc8caf301bf978de2ad5ae8cf8bdc3c56f815
abyad-tif <lapajung50@gmail.com>
2023-05-24 11:12:11 +0800

    memperbaiki login page

---
+++ resources/views/inbound.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.sidebar')
+
+@section('css')
+<link rel="stylesheet" href="style/mahasiswa.css">
+@endsection
+
+@section('main')
+        <div class="cards">
+            <div class="cards_head">
+                <h1>Data Mahasiswa Inbound</h1>
+                <span class="divider divider--color"></span>
+            </div>
+            <div class="cards_body">
+                <form
+                class="d-inline-block mb-3 navbar-search ">
+                <div class="input-group d-flex">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                        aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            @if (Session::has('success'))
+            <div class="alert alert-success" role="alert">
+              {{Session::get('success')}}
+            </div>
+            @endif
+            <div class="table-responsive">
+              <table id="example" class="table table-striped table-hover ">
+                <thead class="table-primary">
+                  <tr >
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Institusi Asal</th>
+                    <th scope="col">Fakultas</th>
+                    <th scope="col">Prodi</th>
+                    <th scope="col">Negara Asal</th>
+                    <th scope="col">Nama Program</th>
+                    <th scope="col">Durasi Program</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">PIC</th>
+                  </tr>
+                </thead>
+                @if($dataInbound -> count() > 0)
+                  @foreach($dataInbound as $inbound)
+                  <tr>
+                    <td class="text-center">{{ $loop-> iteration }}</td>
+                    <td> {{ $inbound-> Nama }}</td>
+                    <td class="text-center">{{ $inbound-> Institusi_Asal }}</td>
+                    <td>{{ $inbound-> Fakultas }}</td>
+                    <td class="text-center">{{ $inbound-> Prodi }}</td>
+                    <td class="text-center">{{ $inbound-> Negara_Asal }}</td>
+                    <td class="text-center">{{ $inbound-> Nama_Program }}</td>
+                    <td class="text-center">{{ $inbound-> Durasi_Program }}</td>
+                    <td>{{ $inbound-> Email }}</td>
+                    <td>{{ $inbound-> PIC }}</td>
+                    <td>
+                      <div class="btn-group" role="group" aria-label="Basic example">
+                      <a  href="{{ route('inbound.edit', $inbound->id)}}" type="button" class="btn btn-primary">Edit</a>
+                      <form action="{{ route('inbound.destroy', $inbound->id) }}" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger m-0">Delete</button>
+
+                      </div>
+                  </td>
+                  </tr>
+                  @endforeach
+                @else
+                <tr>
+                  <td class ="text-center"colspan="14">Tidak ada data mahasiswa</td>
+                </tr>
+                @endif
+                </tbody>
+              </table>
+            </div>
+                  <a class="btn btn-primary" href="{{route('tambah_inbound')}}" role="button">Tambah Mahasiswa</a>
+            </div>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-end">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+              </nav>
+        </div>
+ @endsection   
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,33 +6,63 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="/style/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/login.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
-  <div class="login__wrapper">
-    <div class="form-box login">
-      <h2>Kantor Urusan Internasional</h2>
-      <form action="/login" method="POST">
-        @csrf 
-        <div class="login__input">
-          <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
-          <input type="text" name="username" id="username" required>
-          <label for="username">Username</label>
+  <section class="login__wrapper d-flex">
+    <div class="login__left w-50 h-100 bg-img">
+      <div class="vh-100 form-box row justify-content-center login align-items-center">
+        <div class="col-6">
+          <!-- <h1>Gambar</h1> -->
         </div>
-        <div class="login__input">
-          <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
-          <input type="password" name="password" id="password" required>
-          <label for="password">Password</label>
-        </div>
-        <button class="buttons" type="submit">Login</button> 
-      </form>
-      <a href="/register">Sign Up</a>
+      </div>
     </div>
-  </div>
+
+    <div class="login__right w-50 vh-100">
+      <div class="vh-100 form-box row justify-content-center login align-items-center">
+        <div class="col-6">
+          <h2 class="text-center">Kantor Urusan Internasional</h2>
+          <form id="login" action="/login" method="POST">
+            @csrf
+            <div class="login__input mb-3">
+              <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
+              <label class="form-label" for="username">Username</label>
+              <input class="form-control" type="text" name="username" id="username" placeholder="Enter Username"
+                required>
+            </div>
+            <div class="login__input mb-3">
+              <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+              <label class="form-label" for="password">Password</label>
+              <input class="form-control" type="password" name="password" id="password" placeholder="Enter Password"
+                required>
+            </div>
+            <button class="buttons mb-3" type="submit">Login</button>
+          </form>
+          <div class="text-center">
+            <span class="d-inline">Don't have an Account? <a class="d-inline" href="/register">Sign Up
+                Here</a></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <script src="https://kit.fontawesome.com/e701facadd.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+    integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
